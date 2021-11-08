7e3c6db0479b6ceab6341e721ddafb586e7ecc85
Your Name <you@example.com>
2021-11-08 23:43:41 +0330

    install typeorm and config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath:[`.env.stage.${process.env.STAGE}`]
+    }),
+    TypeOrmModule.forRootAsync({
+      imports:[ConfigModule],
+      inject:[ConfigService],
+      useFactory:async(configService:ConfigService)=>{
+        return {
+          type:"postgres",
+          autoLoadEntities:true,
+          username:configService.get("DB_USERNAME"),
+          password:configService.get("DB_PASSWORD"),
+          database:configService.get("DB_NAME"),
+          host:configService.get("DB_HOST"),
+          port:configService.get("DB_PORT"),
+          synchronize:configService.get("DB_SYNC")
+        }
+      }
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
